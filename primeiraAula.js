1af64146eed53de82eac0c913158fbeaa4ed906d
giuliasb296 <123772160+giuliasb296@users.noreply.github.com>
2023-07-26 13:45:13 -0300

    Add files via upload
    Primeira aula de javascript.

---
+++ primeiraAula.js
@@ -0,0 +1,57 @@
+console.log('16') //Atividade 1 - Resultado: 16
+
+console.log('5 semanas') //Atividade 2 - Resultado: 5 semanas.
+
+console.log('Atividade 3') /* Essa é a atividade 3, que solicita 
+                           comentários em várias linhas.
+                           */
+
+let number = 10.5; //varivável tipo number 
+
+let string = 'Bem-Vindo'; //variável tipo string
+
+let boolean = true; //variável tipo boolean
+
+let nulo = null; //variável tipo null
+
+const object = {           //variável tipo object
+    name: 'Giulia',
+    age: 16,
+    isActive: true,
+};
+
+let indefinido = undefined; //variável tipo undefined
+
+
+
+console.log(16+3.5) //Atividade 5 - Resultado: 19.5
+
+let now = new Date                      //Atividade 6 - Resultado 54
+console.log(now.getFullYear()- 1969) 
+
+console.log(65/240) //Atividade 7 - Resultado: 0.2708333333
+
+console.log(0.2708*100) //Atividade 8 - Resultado: 27,08
+
+console.log('Hello' + 'World!') //Atividade 9 - Resultado: HelloWorld!
+
+console.log('Hello' + ' World!') //Atividade 10 - Resultado: Hello World!
+
+console.log('Teaching the world how to code'.length) //Atividade 11 - Resultado: 30
+
+console.log('Kaian Novais'.toUpperCase()) //Atividade 12 - Resultado: KAIAN NOVAIS
+
+console.log(" Professor Kaian Novais ".trim()) //Atividade 13 - Resultado: Professor Kaian Novais
+
+function getRandom() {              //Atividade 14 
+    return Math.random() * 100;
+  }
+
+  let Arredondar = Math.ceil(43.8)  //Atividade 15 - Resultado: 44
+  console.log(Arredondar)  
+
+  let NInteiro = Number.isInteger(2017) //Atividade 16 - Resultado: true
+  console.log(NInteiro)  
+
+
+
